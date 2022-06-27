1a68726ea36e6eebd8c4b282dc8f93c644203300
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2022-06-28 00:33:30 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-red-600">
+    <p>Welcome to the Dashboard</p>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
